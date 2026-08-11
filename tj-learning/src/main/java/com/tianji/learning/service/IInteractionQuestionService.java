e01761c0791d14c9c4877f09e061b2d14c381d62
ThirteenAsh <1984885454@qq.com>
2026-08-11 20:53:47 +0800

    feat(learning):  完成用户端分页查询评论数据和新增互动问题

---
+++ tj-learning/src/main/java/com/tianji/learning/service/IInteractionQuestionService.java
@@ -0,0 +1,25 @@
+package com.tianji.learning.service;
+
+import com.tianji.common.domain.dto.PageDTO;
+import com.tianji.learning.domain.dto.QuestionFormDTO;
+import com.tianji.learning.domain.po.InteractionQuestion;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tianji.learning.domain.query.QuestionPageQuery;
+import com.tianji.learning.domain.vo.QuestionVO;
+
+import javax.validation.Valid;
+
+/**
+ * <p>
+ * 互动提问的问题表 服务类
+ * </p>
+ *
+ * @author ThirteenAsh
+ * @since 2026-08-11
+ */
+public interface IInteractionQuestionService extends IService<InteractionQuestion> {
+
+    void saveQuestion(@Valid QuestionFormDTO questionDTO);
+
+    PageDTO<QuestionVO> queryQuestionPage(QuestionPageQuery query);
+}
---
+++ tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionQuestionServiceImpl.java
@@ -0,0 +1,133 @@
+package com.tianji.learning.service.impl;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.tianji.api.client.user.UserClient;
+import com.tianji.api.dto.user.UserDTO;
+import com.tianji.common.domain.dto.PageDTO;
+import com.tianji.common.exceptions.BadRequestException;
+import com.tianji.common.utils.BeanUtils;
+import com.tianji.common.utils.CollUtils;
+import com.tianji.common.utils.UserContext;
+import com.tianji.learning.domain.dto.QuestionFormDTO;
+import com.tianji.learning.domain.po.InteractionQuestion;
+import com.tianji.learning.domain.po.InteractionReply;
+import com.tianji.learning.domain.query.QuestionPageQuery;
+import com.tianji.learning.domain.vo.QuestionVO;
+import com.tianji.learning.mapper.InteractionQuestionMapper;
+import com.tianji.learning.mapper.InteractionReplyMapper;
+import com.tianji.learning.service.IInteractionQuestionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * <p>
+ * 互动提问的问题表 服务实现类
+ * </p>
+ *
+ * @author ThirteenAsh
+ * @since 2026-08-11
+ */
+@Service
+@RequiredArgsConstructor
+public class InteractionQuestionServiceImpl extends ServiceImpl<InteractionQuestionMapper, InteractionQuestion> implements IInteractionQuestionService {
+
+    private final InteractionReplyMapper replyMapper;
+    private final UserClient userClient;
+
+    @Override
+    public void saveQuestion(QuestionFormDTO questionDTO) {
+        // 1.获取当前登录的用户id
+        Long userId = UserContext.getUser();
+        // 2.数据封装
+        InteractionQuestion question = BeanUtils.copyBean(questionDTO, InteractionQuestion.class);
+        question.setUserId(userId);
+        // 3.写入数据库
+        save(question);
+    }
+
+    @Override
+    public PageDTO<QuestionVO> queryQuestionPage(QuestionPageQuery query) {
+        // 1.参数校验，课程id和小节id不能都为空
+        Long courseId = query.getCourseId();
+        Long sectionId = query.getSectionId();
+        if (courseId == null && sectionId == null) {
+            throw new BadRequestException("课程id和小节id不能都为空");
+        }
+        // 2.分页查询
+        Page<InteractionQuestion> page = lambdaQuery()
+                .select(InteractionQuestion.class, info -> !info.getProperty().equals("description"))
+                .eq(query.getOnlyMine(), InteractionQuestion::getUserId, UserContext.getUser())
+                .eq(courseId != null, InteractionQuestion::getCourseId, courseId)
+                .eq(sectionId != null, InteractionQuestion::getSectionId, sectionId)
+                .eq(InteractionQuestion::getHidden, false)
+                .page(query.toMpPageDefaultSortByCreateTimeDesc());
+        List<InteractionQuestion> records = page.getRecords();
+        if (CollUtils.isEmpty(records)) {
+            return PageDTO.empty(page);
+        }
+        // 3.根据id查询提问者和最近一次回答的信息
+        Set<Long> userIds = new HashSet<>();
+        Set<Long> answerIds = new HashSet<>();
+        // 3.1.得到问题当中的提问者id和最近一次回答的id
+        for (InteractionQuestion q : records) {
+            if(!q.getAnonymity()) { // 只查询非匿名的问题
+                userIds.add(q.getUserId());
+            }
+            answerIds.add(q.getLatestAnswerId());
+        }
+        // 3.2.根据id查询最近一次回答
+        answerIds.remove(null);
+        Map<Long, InteractionReply> replyMap = new HashMap<>(answerIds.size());
+        if(CollUtils.isNotEmpty(answerIds)) {
+            List<InteractionReply> replies = replyMapper.selectBatchIds(answerIds);
+            for (InteractionReply reply : replies) {
+                replyMap.put(reply.getId(), reply);
+                if(!reply.getAnonymity()){
+                    userIds.add(reply.getUserId());
+                }
+            }
+        }
+
+        // 3.3.根据id查询用户信息（提问者）
+        userIds.remove(null);
+        Map<Long, UserDTO> userMap = new HashMap<>(userIds.size());
+        if(CollUtils.isNotEmpty(userIds)) {
+            List<UserDTO> users = userClient.queryUserByIds(userIds);
+            userMap = users.stream()
+                    .collect(Collectors.toMap(UserDTO::getId, u -> u));
+        }
+
+        // 4.封装VO
+        List<QuestionVO> voList = new ArrayList<>(records.size());
+        for (InteractionQuestion r : records) {
+            // 4.1.将PO转为VO
+            QuestionVO vo = BeanUtils.copyBean(r, QuestionVO.class);
+            voList.add(vo);
+            // 4.2.封装提问者信息
+            if(!r.getAnonymity()){
+                UserDTO userDTO = userMap.get(r.getUserId());
+                if (userDTO != null) {
+                    vo.setUserName(userDTO.getName());
+                    vo.setUserIcon(userDTO.getIcon());
+                }
+            }
+
+            // 4.3.封装最近一次回答的信息
+            InteractionReply reply = replyMap.get(r.getLatestAnswerId());
+            if (reply != null) {
+                vo.setLatestReplyContent(reply.getContent());
+                if(!reply.getAnonymity()){
+                    UserDTO user = userMap.get(reply.getUserId());
+                    vo.setLatestReplyUser(user.getName());
+                }
+
+            }
+        }
+
+        return PageDTO.of(page, voList);
+    }
+}
