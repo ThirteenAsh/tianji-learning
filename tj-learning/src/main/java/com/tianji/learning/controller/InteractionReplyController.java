973dedeec81e8437227f2fec0cac0331de701f8d
ThirteenAsh <1984885454@qq.com>
2026-08-12 15:48:45 +0800

    feat(learning):  完成互动回答分页查询功能

--- tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyController.java
@@ -1,15 +1,14 @@
 package com.tianji.learning.controller;
 
 
+import com.tianji.common.domain.dto.PageDTO;
 import com.tianji.learning.domain.dto.ReplyDTO;
+import com.tianji.learning.domain.vo.ReplyVO;
+import com.tianji.learning.domain.query.ReplyPageQuery;
 import com.tianji.learning.service.IInteractionReplyService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -30,5 +29,11 @@ public class InteractionReplyController {
     @PostMapping
     public void saveReply(@RequestBody ReplyDTO replyDTO) {
         replyService.saveReply(replyDTO);
+    }
+
+    @ApiOperation("分页查询回答或评论")
+    @GetMapping("page")
+    public PageDTO<ReplyVO> queryReplyPage(ReplyPageQuery pageQuery){
+        return replyService.queryReplyPage(pageQuery, false);
     }
 }
