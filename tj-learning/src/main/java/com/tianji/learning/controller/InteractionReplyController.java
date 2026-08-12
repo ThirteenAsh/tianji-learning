53d7b7ed969ffe22780295f6aedea2a38a0b9c54
ThirteenAsh <1984885454@qq.com>
2026-08-12 14:48:46 +0800

    feat(learning):  完成新增回答或评论

--- tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyController.java
@@ -1,6 +1,12 @@
 package com.tianji.learning.controller;
 
 
+import com.tianji.learning.domain.dto.ReplyDTO;
+import com.tianji.learning.service.IInteractionReplyService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2026-08-11
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/replies")
 public class InteractionReplyController {
 
+    private final IInteractionReplyService replyService;
+
+    @ApiOperation("新增回答或评论")
+    @PostMapping
+    public void saveReply(@RequestBody ReplyDTO replyDTO) {
+        replyService.saveReply(replyDTO);
+    }
 }
--- tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionReplyServiceImpl.java
+++ tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionReplyServiceImpl.java
@@ -1,10 +1,19 @@
 package com.tianji.learning.service.impl;
 
+import com.tianji.api.client.user.UserClient;
+import com.tianji.common.utils.BeanUtils;
+import com.tianji.common.utils.UserContext;
+import com.tianji.learning.domain.dto.ReplyDTO;
 import com.tianji.learning.domain.po.InteractionReply;
+import com.tianji.learning.domain.po.InteractionQuestion;
+import com.tianji.learning.enums.QuestionStatus;
 import com.tianji.learning.mapper.InteractionReplyMapper;
+import com.tianji.learning.service.IInteractionQuestionService;
 import com.tianji.learning.service.IInteractionReplyService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
@@ -15,6 +24,38 @@ import org.springframework.stereotype.Service;
  * @since 2026-08-11
  */
 @Service
+@RequiredArgsConstructor
 public class InteractionReplyServiceImpl extends ServiceImpl<InteractionReplyMapper, InteractionReply> implements IInteractionReplyService {
 
+    private final IInteractionQuestionService questionService;
+    private final UserClient userClient;
+
+    @Override
+    @Transactional
+    public void saveReply(ReplyDTO replyDTO) {
+        // 1.获取登录用户
+        Long userId = UserContext.getUser();
+        // 2.新增回答
+        InteractionReply reply = BeanUtils.toBean(replyDTO, InteractionReply.class);
+        reply.setUserId(userId);
+        save(reply);
+        // 3.累加评论数或者累加回答数
+        // 3.1.判断当前回复的类型是否是回答
+        boolean isAnswer = replyDTO.getAnswerId() == null;
+        if (!isAnswer) {
+            // 3.2.是评论，则需要更新上级回答的评论数量
+            lambdaUpdate()
+                    .setSql("reply_times = reply_times + 1")
+                    .eq(InteractionReply::getId, replyDTO.getAnswerId())
+                    .update();
+        }
+        // 3.3.尝试更新问题表中的状态、 最近一次回答、回答数量
+        questionService.lambdaUpdate()
+                .set(isAnswer, InteractionQuestion::getLatestAnswerId, reply.getAnswerId())
+                .setSql(isAnswer, "answer_times = answer_times + 1")
+                .set(replyDTO.getIsStudent(), InteractionQuestion::getStatus, QuestionStatus.UN_CHECK.getValue())
+                .eq(InteractionQuestion::getId, replyDTO.getQuestionId())
+                .update();
+
+    }
 }
